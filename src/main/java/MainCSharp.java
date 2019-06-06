46e3d3204c8ed173af9ebb2ee47e3bde92abb872
Adrian Opiela <adrian.opiela11@gmail.com>
2019-06-06 22:03:36 +0200

    FileWritten

--- src/main/java/MainCSharp.java
+++ src/main/java/MainCSharp.java
@@ -3,32 +3,26 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
+import java.io.*;
 import java.util.Scanner;
 
 
 public class MainCSharp {
     public static void main(String[] args) {
         try {
-            String tmp = "float b = 10.36; int b; char b = 'h'; double fff; double dddd = 120.456;";
-            //TODO testowe przykłady do ifa
-            // if(a+3+3+k+2>4*h+3.0*4.0) {if(d-6>y) {int a = 10;} else { int b = 10;}} else {int c = 7;}
-            //for(int i = 0;a+b+5<b;i++) {int a = 10;}
-            //for(int i = 0 ; true ; i++) {int a = 10;}
-            //for(int i = 0 ; a-9<i+9 ; i++) {if(d-6>y) {int a = 10;} else { int b = 10;}}
-            //while(true){for(int i = 0 ; a-9<i+9 ; i++) {if(d-6>y) {int a = 10;} else { for(int i = 0 ; a-9<i+9 ; i++) {if(d-6>y) {int a = 10;} else { int b = 10;}}}}}
-//            while(a>9){ a = a+10+63;}
-//            while(a+b+7>78+l){int a = 10;}
-//            do{int a = 10+69; b=a+96+c;}while(b>6)
-//            int a;
-//            a = 75 + 96;
-            // if(a+b+c > 6+9){int a = 10;}
+
+            String toTranslate="";
 
 
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Podaj kod  C# do translacji:");
 
-            tmp = scanner.nextLine();
-            ANTLRInputStream input = new ANTLRInputStream(tmp);
+            /// Wczytywane z consoli
+//            Scanner scanner = new Scanner(System.in);
+//            System.out.println("Podaj kod  C# do translacji:");
+//            toTranslate = scanner.nextLine();
+
+            ///Wczytywanie z pliku
+            toTranslate = readFile();
+            ANTLRInputStream input = new ANTLRInputStream(toTranslate);
             CSharpLexer lexer = new CSharpLexer(input);
             CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
             CSharpParser parser = new CSharpParser(commonTokenStream);
@@ -38,10 +32,40 @@ public class MainCSharp {
             ParseTreeWalker walker = new ParseTreeWalker();
             CSharpBaseListener listener = new CSharpBaseListener();
             walker.walk(listener, tree);
-            System.out.println(listener.getTranslate());
+            String translated = listener.getTranslate();
+            System.out.println(translated);
+            writeToFile(translated);
+
+
+
 
         } catch (Exception e) {
             System.err.println("exception: " + e);
         }
+    }
+
+    public static void writeToFile(String string) throws FileNotFoundException {
+        PrintWriter printWriter = new PrintWriter("translated.txt");
+        for (String s : string.split("\n")) {
+            printWriter.println(s);
+
+        }
+        printWriter.close();
+    }
+
+    public static String readFile() throws IOException {
+        String toTranslate="";
+
+        BufferedReader reader = new BufferedReader(new FileReader("toTranslate.txt"));
+        String line = reader.readLine();
+        while (line != null) {
+            System.out.println(line);
+            toTranslate+=line+" ";
+            line = reader.readLine();
+        }
+        String afterTrim = toTranslate.trim().replaceAll(" +", " ");
+        System.out.println(afterTrim);
+        return afterTrim;
+
     }
 }
